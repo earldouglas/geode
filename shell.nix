01a2d8b80125dee555845f3d2f319f70d33b75dd
James Earl Douglas <james@earldouglas.com>
2023-11-04 18:18:25 -0700

    Add a simple Nix shell configuration (#15)
    Fixes #10

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  derivations =
+    pkgs.fetchFromGitHub {
+      owner = "earldouglas";
+      repo = "derivations";
+      rev = "007b06268778bc86def0f77f986556789db6e539";
+      sha256 = "0k34hbvmvc24gwjkc680bm45ss0bv5dklahgr8adqkg46zi3kkwd";
+    };
+  nvim-haskell = import (derivations + "/nvim/haskell") {};
+in
+pkgs.mkShell {
+  nativeBuildInputs = [
+    nvim-haskell
+    pkgs.haskell-language-server
+    pkgs.cabal-install
+    pkgs.zlib
+  ];
+}
